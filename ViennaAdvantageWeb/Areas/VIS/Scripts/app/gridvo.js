38f23ebb3efeb91453ce31517196bfab57469b5f
Harwinder-VA <harwinder.gill@viennasolutions.com>
2019-10-03 12:54:18 +0530

    gitignore

--- ViennaAdvantageWeb/Areas/VIS/Scripts/app/gridvo.js
+++
@@ -1,7 +0,0 @@
-﻿; (function (VIS, $) {
-
-
-
-
-
-})(VIS, JQuery);
--- ViennaAdvantageWeb/Areas/ViennaBase/Scripts/crystalreportviewers13/js/crviewer/dom.js
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) Business Objects 2006. All rights reserved. */
-
-if (typeof(bobj.dom) == 'undefined') {
-    bobj.dom = {};    
-}
-
