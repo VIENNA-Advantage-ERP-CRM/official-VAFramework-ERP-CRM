c4361c2ce4736f7d2043aa0884ad5da12d58bc91
karanvienna <karan@viennasolutions.com>
2019-10-31 15:16:15 +0530

    Revert "Header Panel related Tasks"
    This reverts commit f64f23c9c8879779ad181220f4841d0f1cd89d4f.

--- VIS/Areas/VIS/Scripts/app/headerPanelFrame.js
+++ VIS/Areas/VIS/Scripts/app/headerPanelFrame.js
@@ -3,6 +3,9 @@
     function HeaderPanel(gTab) {
         var $root = null;
 
+
+
+
         var setHeaderLayout = function () {
             if (gTab.getIsHeaderPanel()) {
                 var headerItems = gTab.getHeaderPanelItems();
@@ -31,58 +34,32 @@
         var setHeaderItems = function (gTab, items) {
 
             var fields = gTab.gridTable.gridFields;
-            for (var i = 0; i < fields.length; i++) {
+
+            for (var i = 0; i < fields.length; i++)
+            {
                 var mField = fields[i];
                 if (mField.getIsDisplayed() && mField.getIsHeaderPanelitem()) {
 
                     var headerItem = items[mField.getHeaderSeqno()];
-                    var startCol = headerItem.StartColumn;
-                    var colSpan = headerItem.ColumnSpan;
-                    var startRow = headerItem.StartRow;
-                    var rowSpan = headerItem.RowSpan;
-                    var div = $('<div style="grid-column: ' + startCol + '/ span ' + colSpan + '; grid-row: ' + startRow + '/ span ' + rowSpan + ';background-color:pink"/>');
 
-                  var  label = "<h3>" + w2utils.encodeTags(field.getHeader()) + "</h3>";
+                    //getHeaderHeadingOnly 
+                    //getHeaderIconOnly
 
-                    var colValue = mField.getValue();
-
-                    if (!colValue || colValue == "" || !mField.getIsDisplayed())
-                        continue;
-
-                    var displayType = mField.getDisplayType();
-
-                    if (mField.lookup) {
-                        colValue = mField.lookup.getDisplay(colValue, true);
-                    }
-
-                    //	Date
-                    else if (VIS.DisplayType.IsDate(displayType)) {
-                        colValue = new Date(colValue).toLocaleString();
-                    }
-
-                    //	YesNo
-                    else if (displayType == VIS.DisplayType.YesNo) {
-                        var str = colValue.toString();
-                        if (mField.getIsEncryptedColumn())
-                            str = VIS.secureEngine.decrypt(str);
-                        colValue = str.equals("Y");	//	Boolean
-                    }
-                    //	LOB 
-
-                    else
-                        colValue = colValue.toString();//string
-                    //	Encrypted
-                    if (mField.getIsEncryptedColumn() && displayType != VIS.DisplayType.YesNo)
-                        colValue = VIS.secureEngine.decrypt(colValue);
-
-
-                    var value = "<h5>" + w2utils.encodeTags(colValue) + "</h5>";
-                    div.append(label).append(value);
-
-                    $root.append(div);
 
                 }
             }
+
+
+
+            //for (var j = 0; j < items.length; j++) {
+
+            //    var startCol = items[j].StartColumn;
+            //    var colSpan = items[j].ColumnSpan;
+            //    var startRow = items[j].StartRow;
+            //    var rowSpan = items[j].RowSpan;
+            //    var div = $('<div style="grid-column: ' + startCol + '/ span ' + colSpan + '; grid-row: ' + startRow + '/ span ' + rowSpan + ';background-color:pink"/>');
+            //    $root.append(div);
+            //}
         };
 
         this.init = function () {
