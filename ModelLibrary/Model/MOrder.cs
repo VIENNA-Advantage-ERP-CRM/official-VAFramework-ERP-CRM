379fe4c168377888a7a86606a1270dc248660190
Bharat-Vienna <bharat.singla@viennasolutions.com>
2019-11-07 18:07:14 +0530

    Blanket order qty not updated correctly by Release order process

--- ModelLibrary/Model/MOrder.cs
+++ ModelLibrary/Model/MOrder.cs
@@ -1292,7 +1292,6 @@ namespace VAdvantage.Model
                     if (docType.IsReleaseDocument())
                     {
                         line.SetC_OrderLine_Blanket_ID(fromLines[i].GetC_OrderLine_ID());
-
                         // Blanket order qty not updated correctly by Release order process
                         line.SetQtyBlanket(fromLines[i].GetQtyOrdered());
                     }
