b9a7e0f1d3b0cfa351a91ebd321aae34ad98c7f1
Amit Bansal <amit.bansal@viennasolutions.com>
2019-11-22 16:47:40 +0530

    Allow Reverse on GL Journal Handle issue of Reverse handle precision handling

--- ModelLibrary/Model/MLineDimension.cs
+++ ModelLibrary/Model/MLineDimension.cs
@@ -9,31 +9,33 @@ using ViennaAdvantage.Model;
 
 namespace VAdvantage.Model
 {
-    public class MLineDimension : X_GL_LineDimension 
+    public class MLineDimension : X_GL_LineDimension
     {
         public MLineDimension(Ctx ctx, int GL_LineDimension_ID, Trx trxName)
              : base(ctx, GL_LineDimension_ID, trxName)
-        { 
+        {
         }
 
-        public MLineDimension(Ctx ctx, DataRow dr, Trx trxName) 
+        public MLineDimension(Ctx ctx, DataRow dr, Trx trxName)
              : base(ctx, dr, trxName)
-        { 
+        {
         }
 
         protected override bool BeforeSave(bool newRecord)
         {
             MJournalLine obj = new MJournalLine(GetCtx(), GetGL_JournalLine_ID(), Get_Trx());
-            string val = "";
-
-            if (obj.GetAmtSourceDr() > 0)
+            // In Case of reversal, bypass this condition
+            if (!(obj.Get_ColumnIndex("ReversalDoc_ID") > 0 && obj.GetReversalDoc_ID() > 0))
             {
-                val = " AmtSourceDr ";
-            }
-            else
-            {
-                val = " AmtSourceCr ";
-            }
+                string val = "";
+                if (obj.GetAmtSourceDr() > 0)
+                {
+                    val = " AmtSourceDr ";
+                }
+                else
+                {
+                    val = " AmtSourceCr ";
+                }
 
                 string sql = "SELECT SUM(amount) FROM Gl_Linedimension WHERE GL_JournalLine_ID=" + Get_Value("GL_JournalLine_ID") + " AND Gl_Linedimension_ID NOT IN( " + GetGL_LineDimension_ID() + ")";
                 Decimal count = Util.GetValueOfDecimal(DB.ExecuteScalar(sql, null, Get_Trx()));
@@ -42,16 +44,17 @@ namespace VAdvantage.Model
                 sql = "SELECT " + val + " FROM GL_JournalLine WHERE GL_JournalLine_ID=" + Get_Value("GL_JournalLine_ID");
                 Decimal amtcount = Util.GetValueOfDecimal(DB.ExecuteScalar(sql, null, Get_Trx()));
 
-            if (count > amtcount)
-            {
-                log.SaveWarning("AmoutCheck", "");
-                return false;
+                if (count > amtcount)
+                {
+                    log.SaveWarning("AmoutCheck", "");
+                    return false;
+                }
             }
 
-          return true;
+            return true;
         }
 
-       
+
 
 
     }
