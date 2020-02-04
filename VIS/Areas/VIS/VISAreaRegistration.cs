76fbd6fb62861e1c45f2cd8519e72cad9dad496b
karanvienna <karan@viennasolutions.com>
2020-02-04 15:45:53 +0530

    Comm

--- VIS/Areas/VIS/VISAreaRegistration.cs
+++ VIS/Areas/VIS/VISAreaRegistration.cs
@@ -150,6 +150,7 @@ namespace VIS
 
                  "~/Areas/VIS/Scripts/app/windowformcontainer.js",
                   "~/Areas/VIS/Scripts/app/masterdataversion.js",
+                  "~/Areas/VIS/Scripts/tabpanels/bipanel.js",
                    "~/Areas/VIS/Scripts/model/CalloutColumn.js",
 
 
