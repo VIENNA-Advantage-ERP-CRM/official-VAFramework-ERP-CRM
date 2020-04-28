50bd28dcd6419acfbea48a9cad14ece83ac1e1e9
harwinder.singh <harwinder.singh@viennasolutions.com>
2020-04-28 14:36:17 +0000

    theme2

--- VIS/Areas/VIS/VISAreaRegistration.cs
+++ VIS/Areas/VIS/VISAreaRegistration.cs
@@ -181,7 +181,8 @@ namespace VIS
                  "~/Areas/VIS/Scripts/F20/aviewcontroller.js",
                  "~/Areas/VIS/Scripts/F20/apanel.js",
                  "~/Areas/VIS/Scripts/F20/acontentpane.js",
-                 "~/Areas/VIS/Scripts/F20/afilterpanel.js"
+                 "~/Areas/VIS/Scripts/F20/afilterpanel.js",
+                 "~/Areas/VIS/Scripts/F20/themecnfgtr.js"
                  );
 
 
@@ -209,7 +210,8 @@ namespace VIS
                 "~/Areas/VIS/Content/F20/Site.css",
                 "~/Areas/VIS/Content/F20/Window.css",
                         "~/Areas/VIS/Content/F20/Controls.css",
-                         "~/Areas/VIS/Content/F20/Media.css"
+                         "~/Areas/VIS/Content/F20/Media.css",
+                         "~/Areas/VIS/Content/F20/ThemeCnfgtr.css"
                 );
 
 
