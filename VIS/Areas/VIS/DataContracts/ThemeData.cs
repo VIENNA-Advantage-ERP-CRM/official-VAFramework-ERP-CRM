efb47ea868d7edb2d36d66dbdafe375ca4ea0036
harwinder.singh <harwinder.singh@viennasolutions.com>
2020-04-29 10:11:12 +0000

    thememodel

---
+++ VIS/Areas/VIS/DataContracts/ThemeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VIS.DataContracts
+{
+    public class ThemeData
+    {
+        public string Primary { get; set; }
+        public string OnPrimary { get; set; }
+        public string Seconadary { get; set; }
+        public string OnSecondary { get; set; }
+        public bool IsDefault { get; set; }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
