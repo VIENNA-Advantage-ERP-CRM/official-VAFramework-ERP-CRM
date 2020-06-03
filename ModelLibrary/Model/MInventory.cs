a3ee67b1735b25a6fa1cbdfbdcbf253f43cbce79
Bharat-Vienna <bharat.singla@viennasolutions.com>
2020-06-03 15:44:30 +0530

    JID_0799, JID_0889, JID_0891, JID_1422

--- ModelLibrary/Model/MInventory.cs
+++ ModelLibrary/Model/MInventory.cs
@@ -3033,7 +3033,9 @@ namespace VAdvantage.Model
             reversal.SetDocStatus(DOCSTATUS_Reversed);
             reversal.SetDocAction(DOCACTION_None);
             reversal.Save();
-            _processMsg = reversal.GetDocumentNo();
+
+            //JID_0889: show on void full message Reversal Document created
+            _processMsg = Msg.GetMsg(GetCtx(), "VIS_DocumentReversed") + reversal.GetDocumentNo();
 
             //	Update Reversed (this)
             AddDescription("(" + reversal.GetDocumentNo() + "<-)");
