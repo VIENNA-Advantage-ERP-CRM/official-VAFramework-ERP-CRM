5f935e3a156b5d75f4458bf8173412e86cf3ed51
amit.bansal@viennasolutions.com <amit.bansal@viennasolutions.com>
2020-12-18 11:59:57 +0530

    Beta Merge -- set revenue recoggnition on invoice line if binded on product

--- VAS/Areas/VAS/Controllers/CallOut/MProductController.cs
+++ VAS/Areas/VAS/Controllers/CallOut/MProductController.cs
@@ -76,7 +76,24 @@ namespace VIS.Controllers
                 retJSON = JsonConvert.SerializeObject(objProduct.GetTaxCategory(ctx, fields));
             }
             return Json(retJSON, JsonRequestBehavior.AllowGet);
-        } 
+        }
         //End
+
+        /// <summary>
+        /// Get C_REVENUERECOGNITION_ID
+        /// </summary>
+        /// <param name="fields">C_Product_ID</param>
+        /// <returns>C_REVENUERECOGNITION_ID</returns>
+        public JsonResult GetRevenuRecognition(string fields)
+        {
+            string retJSON = "";
+            if (Session["ctx"] != null)
+            {
+                Ctx ctx = Session["ctx"] as Ctx;
+                MProductModel objProduct = new MProductModel();
+                retJSON = JsonConvert.SerializeObject(objProduct.GetRevenuRecognition(ctx, fields));
+            }
+            return Json(retJSON, JsonRequestBehavior.AllowGet);
+        }
     }
 }
