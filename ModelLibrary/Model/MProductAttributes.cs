84b50298830387251538532046d8a03664ac1817
Bharat Singla <bharat.singla@viennasolutions.com>
2020-12-23 16:16:43 +0530

    Handle issue on delete of Product Attribute

--- ModelLibrary/Model/MProductAttributes.cs
+++ ModelLibrary/Model/MProductAttributes.cs
@@ -116,7 +116,7 @@ namespace VAdvantage.Model
                 + GetM_AttributeSetInstance_ID() + " UNION SELECT COUNT(*) AS cc FROM M_InventoryLine WHERE M_Product_ID = " + GetM_Product_ID() + " AND M_AttributeSetInstance_ID = "
                 + GetM_AttributeSetInstance_ID() + " UNION SELECT COUNT(*) AS cc FROM C_OrderLine WHERE M_Product_ID = " + GetM_Product_ID() + " AND M_AttributeSetInstance_ID = "
                 + GetM_AttributeSetInstance_ID() + " UNION  SELECT COUNT(*) AS cc FROM M_InOutLine WHERE M_Product_ID = " + GetM_Product_ID() + " AND M_AttributeSetInstance_ID = "
-                + GetM_AttributeSetInstance_ID() + ")";
+                + GetM_AttributeSetInstance_ID() + ") t";
             int no = Util.GetValueOfInt(DB.ExecuteScalar(uqry));
             if (no > 0)
             {
