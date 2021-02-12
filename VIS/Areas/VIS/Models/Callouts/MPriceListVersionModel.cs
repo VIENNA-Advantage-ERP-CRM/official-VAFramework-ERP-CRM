905f0da8d12f9929102cd3657d5c60ad58c08851
Kumar <kumar.p@viennasolutions.com>
2021-02-12 14:40:07 +0100

    Pricelist - QA change : handling inactive records

--- VIS/Areas/VIS/Models/Callouts/MPriceListVersionModel.cs
+++ VIS/Areas/VIS/Models/Callouts/MPriceListVersionModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using VAdvantage.DataBase;
+using VAdvantage.Logging;
 using VAdvantage.Model;
 using VAdvantage.Utility;
 
@@ -12,7 +13,7 @@ namespace VIS.Models
     /// Screen Type enum
     /// </summary>
     public enum ScreenType
-    { 
+    {
         Order = 1,
         Invoice = 2,
         Requisition = 3,
@@ -26,6 +27,16 @@ namespace VIS.Models
         private string _tableName = string.Empty;
         private string _keyColumnName = string.Empty;
         private string _columnName = string.Empty;
+        /**	Logger			*/
+        protected VLogger log = null;
+
+        public MPriceListVersionModel()
+        {
+            if (log == null)
+            {
+                log = VLogger.GetVLogger(this.GetType().FullName);
+            }
+        }
 
         /// <summary>
         /// Get Price List Version ID
@@ -36,13 +47,13 @@ namespace VIS.Models
         public int GetM_PriceList_Version_ID(Ctx ctx, string fields)
         {
             /** Price List - ValidFrom date validation ** Dt:01/02/2021 ** Modified By: Kumar **/
-            int M_PriceList_ID=0, transactionId = 0, productId=0, uomId=0, attrSetInstId=0, tranType=0;
+            int M_PriceList_ID = 0, transactionId = 0, productId = 0, uomId = 0, attrSetInstId = 0, tranType = 0;
 
             if (!string.IsNullOrEmpty(fields))
             {
                 string[] paramValue = fields.Split(',');
                 M_PriceList_ID = Util.GetValueOfInt(paramValue[0].ToString());
-                if(paramValue.Length >= 2)
+                if (paramValue.Length >= 2)
                     transactionId = Util.GetValueOfInt(paramValue[1].ToString());
                 if (paramValue.Length >= 3)
                     productId = Util.GetValueOfInt(paramValue[2].ToString());
@@ -53,7 +64,7 @@ namespace VIS.Models
                 if (paramValue.Length >= 6)
                 {
                     tranType = Util.GetValueOfInt(paramValue[5].ToString());
-                    SetTableAndColumnName((ScreenType)tranType);
+                    SetScreenTableAndColumnNames((ScreenType)tranType);
                 }
             }
 
@@ -81,16 +92,16 @@ namespace VIS.Models
         /// <returns>Price List Version ID</returns>
         public int GetM_PriceList_Version_ID(Ctx ctx, string fields, ScreenType screenType)
         {
-            SetTableAndColumnName(screenType);
+            SetScreenTableAndColumnNames(screenType);
 
             return GetM_PriceList_Version_ID(ctx, fields);
         }
 
         /// <summary>
-        /// Set Table And ColumnName
+        /// Set Screen Table And ColumnName
         /// </summary>
         /// <param name="screenType">Screen Type</param>
-        private void SetTableAndColumnName(ScreenType screenType)
+        private void SetScreenTableAndColumnNames(ScreenType screenType)
         {
             if (screenType == ScreenType.Invoice)
             {
@@ -142,62 +153,91 @@ namespace VIS.Models
         /// <returns>Price List Version Id</returns>
         public int GetM_PriceList_Version_ID(Ctx ctx, string priceListId, string transactionId, string productId, string uomId, string attrSetInstId)
         {
-            if (Util.GetValueOfInt(transactionId) > 0)
+            int priceListVersionId = 0;
+            const int UOM_EACH = 100;
+
+            try
             {
-                sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
-                    + @" AND plv.VALIDFROM <= (SELECT " + _columnName + " FROM " + _tableName + " WHERE " + _keyColumnName + "=" + Util.GetValueOfInt(transactionId)
-                    + ") ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
-
-                if (Util.GetValueOfInt(productId) > 0)
+                if (Util.GetValueOfInt(transactionId) > 0)
                 {
-                    sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
-                        + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
-                        + " WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
-                        + @" AND plv.VALIDFROM <= (SELECT " + _columnName + " FROM " + _tableName + " WHERE " + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
-                        + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
-                        + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                    if (Util.GetValueOfInt(productId) > 0)
+                    {
+                        if (Util.GetValueOfInt(uomId) > 0 && Util.GetValueOfInt(attrSetInstId) > 0)
+                        {
+                            sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
+                                    + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
+                                    + " WHERE plv.IsActive = 'Y' AND pp.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                    + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
+                                    + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
+                                    + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = " + Util.GetValueOfInt(attrSetInstId)
+                                    + " AND NVL(pp.C_UOM_ID, 0) = " + Util.GetValueOfInt(uomId) 
+                                    + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                        }
+                        else if (Util.GetValueOfInt(uomId) > 0 && Util.GetValueOfInt(attrSetInstId) <= 0)
+                        {
+                            sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
+                                    + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
+                                    + " WHERE plv.IsActive = 'Y' AND pp.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                    + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
+                                    + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
+                                    + " AND NVL(pp.C_UOM_ID, 0) = " + Util.GetValueOfInt(uomId)
+                                    + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = 0"
+                                    + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                        }
+                        else if (Util.GetValueOfInt(attrSetInstId) > 0 && Util.GetValueOfInt(uomId) <= 0)
+                        {
+                            sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
+                                    + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
+                                    + " WHERE plv.IsActive = 'Y' AND pp.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                    + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
+                                    + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
+                                    + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = " + Util.GetValueOfInt(attrSetInstId)
+                                    + " AND NVL(pp.C_UOM_ID, 0) = 0"
+                                    + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                        }
+                        else
+                        {
+                            sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
+                                    + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
+                                    + " WHERE plv.IsActive = 'Y' AND pp.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                    + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
+                                    + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
+                                    + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = 0"
+                                    + " AND NVL(pp.C_UOM_ID, 0) = 0"
+                                    + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                        }
+                    }
+                    else
+                    {
+                        sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId)
+                                + ") ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                    }
 
-                    if (Util.GetValueOfInt(uomId) > 0)
-                    {                      
+                    priceListVersionId = Util.GetValueOfInt(DB.ExecuteScalar(sql));
+
+                    if (Util.GetValueOfInt(uomId) > 0 && Util.GetValueOfInt(uomId) != Util.GetValueOfInt(UOM_EACH) && priceListVersionId==0 && Util.GetValueOfInt(attrSetInstId)<=0)
+                    {
                         sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
                                 + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
-                                + " WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
-                                + @" AND plv.VALIDFROM <= (SELECT " + _columnName + " FROM " + _tableName + " WHERE " + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
-                                + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)                                
-                                + " AND NVL(pp.C_UOM_ID, 0) = " + Util.GetValueOfInt(uomId)
+                                + " WHERE plv.IsActive = 'Y' AND pp.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
+                                + @" AND plv.VALIDFROM <= (SELECT t." + _columnName + " FROM " + _tableName + " t WHERE t.IsActive = 'Y' AND t." + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
+                                + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
+                                + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = 0"
+                                + " AND NVL(pp.C_UOM_ID, 0) = " + Util.GetValueOfInt(UOM_EACH)
                                 + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
-                    }
 
-                    if (Util.GetValueOfInt(attrSetInstId) > 0)
-                    {
-                        sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
-                            + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
-                            + " WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
-                            + @" AND plv.VALIDFROM <= (SELECT " + _columnName + " FROM " + _tableName + " WHERE " + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
-                            + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
-                            + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = " + Util.GetValueOfInt(attrSetInstId)
-                            + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
-                    }
-
-                    if (Util.GetValueOfInt(uomId) > 0 && Util.GetValueOfInt(attrSetInstId) > 0)
-                    {
-                        sql = "SELECT plv.M_PriceList_Version_ID FROM M_PriceList_Version plv "
-                            + " JOIN M_ProductPrice pp ON plv.M_PriceList_Version_ID = pp.M_PriceList_Version_ID "
-                            + " WHERE plv.IsActive = 'Y' AND plv.M_PriceList_ID = " + priceListId
-                            + @" AND plv.VALIDFROM <= (SELECT " + _columnName + " FROM " + _tableName + " WHERE " + _keyColumnName + "=" + Util.GetValueOfInt(transactionId) + ") "
-                            + " AND NVL(pp.M_Product_ID, 0) = " + Util.GetValueOfInt(productId)
-                            + " AND NVL(pp.M_AttributeSetInstance_ID, 0) = " + Util.GetValueOfInt(attrSetInstId)
-                            + " AND NVL(pp.C_UOM_ID, 0) = " + Util.GetValueOfInt(uomId)
-                            + " ORDER BY plv.VALIDFROM DESC, plv.M_PriceList_Version_ID DESC";
+                        priceListVersionId = Util.GetValueOfInt(DB.ExecuteScalar(sql));
                     }
                 }
-                                
-                return Util.GetValueOfInt(DB.ExecuteScalar(sql));
             }
-            else
+            catch (Exception ex)
             {
-                return 0;
+                log.SaveError("Error in GetM_PriceList_Version_ID()", ex);
+                return Util.GetValueOfInt(priceListVersionId);
             }
+
+            return Util.GetValueOfInt(priceListVersionId);
         }
 
         // Added by Bharat on 12/May/2017
