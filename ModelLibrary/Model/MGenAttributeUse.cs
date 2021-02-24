1fbaeaa98ff541ff73016aa8edfe00b61fb30bdf
vinadvantage@outlook.com <vinadvantage@outlook.com>
2021-02-24 07:23:36 +0100

    Fixed DMS attribute for advance search and order by for attribute instance used in DMS

--- ModelLibrary/Model/MGenAttributeUse.cs
+++ ModelLibrary/Model/MGenAttributeUse.cs
@@ -7,21 +7,24 @@ using VAdvantage.DataBase;
 
 namespace VAdvantage.Model
 {
-    public class MGenAttributeUse: X_C_GenAttributeUse
+    public class MGenAttributeUse : X_C_GenAttributeUse
     {
         public MGenAttributeUse(Ctx ctx, int C_GenAttributeValue_ID, Trx trxName)
             : base(ctx, C_GenAttributeValue_ID, trxName)
         {
-
+            if (C_GenAttributeValue_ID != 0)
+            {
+                throw new Exception("Multi-Key");
+            }
         }
 
-         /// <summary>
+        /// <summary>
         /// Load Constructor
         /// </summary>
         /// <param name="ctx"></param>
         /// <param name="idr"></param>
         /// <param name="trxName"></param>
-        public MGenAttributeUse(Ctx ctx, IDataReader idr, Trx trxName)
+        public MGenAttributeUse(Ctx ctx, DataRow idr, Trx trxName)
             : base(ctx, idr, trxName)
         {
 
@@ -29,5 +32,5 @@ namespace VAdvantage.Model
 
 
     }
-    
+
 }
