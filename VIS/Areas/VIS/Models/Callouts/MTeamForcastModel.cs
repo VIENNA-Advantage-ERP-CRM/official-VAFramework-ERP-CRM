8c6d6e8040d113939bb6c63bb29db15994068952
Kavita Salaria <kavita.salaria@viennasolutions.com>
2021-04-15 16:55:00 +0530

    callouts and add process - team forecast

---
+++ VIS/Areas/VIS/Models/Callouts/MTeamForcastModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VAdvantage.DataBase;
+using VAdvantage.Utility;
+
+namespace VIS.Models
+{
+    public class MTeamForcastModel
+    {
+        /// <summary>
+        /// Get SuperVisor
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="fields"></param>
+        /// <returns></returns>
+        public int GetSupervisor(Ctx ctx, int C_Team_ID)
+        {
+           string sql="SELECT SUpervisor_ID FROM C_Team WHERE C_Team_ID="+ C_Team_ID;
+            return Util.GetValueOfInt(DB.ExecuteScalar(sql));       
+        }
+    }
+}
