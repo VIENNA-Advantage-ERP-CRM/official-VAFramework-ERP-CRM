d770103a81d3684786abffb71da4652edc5ada5a
karanvienna <49510501+karanvienna@users.noreply.github.com>
2021-06-01 13:57:29 +0200

    Vis007 card email (#941)
    * Issue resolved for duplicate email in email form.
    
    * Commented

--- VIS/Areas/VIS/Scripts/F20/aviewcontroller.js
+++ VIS/Areas/VIS/Scripts/F20/aviewcontroller.js
@@ -1369,7 +1369,8 @@ VIS.GridController.prototype.checkInsertNewRow = function () {
             this.settingGridSelecton = false;
             this.vCardView.navigate(recid, !this.isCardRow);
         }
-        else if (recid > -1) {
+        // Make sure this code works wonly when card view is opened.
+        else if (recid > -1 && this.isCardRow) {
             this.settingGridSelecton = true;
             this.vTable.select(recid) //select row for Grid
             this.settingGridSelecton = false;
