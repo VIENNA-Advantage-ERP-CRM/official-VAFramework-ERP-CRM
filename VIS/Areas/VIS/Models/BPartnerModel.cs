3506b2ee5e42690d19f066c025d8c477a06974f4
VALokeshChauhan <lokesh.chauhan@viennasolutions.com>
2021-08-17 15:25:11 +0530

    bp form changes

--- VIS/Areas/VIS/Models/BPartnerModel.cs
+++ VIS/Areas/VIS/Models/BPartnerModel.cs
@@ -130,8 +130,9 @@ namespace VIS.Models
 
             /************************************/
             _bpGroup = FillBPGroup();
-            _bpRelation = FillBPRelation();
-            _bpLocation = FillBPLocation(0, ctx);
+            // VIS0060
+            //_bpRelation = FillBPRelation();
+            //_bpLocation = FillBPLocation(0, ctx);
             /************************************/
             ro = _readOnly;
             if (!ro)
