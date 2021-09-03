bdeb51ff9912fc0df14b43c8197cbf50e8d22c21
KavitaSalaria <kavita.salaria@viennasolutions.com>
2021-09-03 12:53:57 +0530

    Commentig

--- VIS/Areas/VIS/Controllers/VCreateFromController.cs
+++ VIS/Areas/VIS/Controllers/VCreateFromController.cs
@@ -193,8 +193,8 @@ namespace VIS.Controllers
             {
                 sql.Append(isBaseLangess);
             }
-            sql.Append(" LEFT OUTER JOIN M_AttributeSetInstance ins ON (ins.M_AttributeSetInstance_ID =l.M_AttributeSetInstance_ID) WHERE l.C_Order_ID=" + (C_Ord_IDs == null ? 0 : C_Ord_IDs) + " AND l.M_Product_ID>0");//Hanlded case: order is not selected and on the change/selection
-                                                                                                                                                                                                                          //of deliverydate excception's coming  missing expression
+            //Hanlded case: order is not selected and on the change/selectionof deliverydate excception's coming  missing expression
+            sql.Append(" LEFT OUTER JOIN M_AttributeSetInstance ins ON (ins.M_AttributeSetInstance_ID =l.M_AttributeSetInstance_ID) WHERE l.C_Order_ID=" + (C_Ord_IDs == null ? 0 : C_Ord_IDs) + " AND l.M_Product_ID>0");
 
             // Get lines from Order based on the setting taken on Tenant to allow non item Product
             if (!forInvoicees && !isAllownonItem)
@@ -244,9 +244,8 @@ namespace VIS.Controllers
                 {
                     sql.Append(isBaseLangess);
                 }
-                                                                                                                                                              
-                sql.Append(" LEFT OUTER JOIN M_AttributeSetInstance ins ON (ins.M_AttributeSetInstance_ID =l.M_AttributeSetInstance_ID) WHERE l.C_Order_ID=" + (C_Ord_IDs == null ? 0 : C_Ord_IDs) + " AND C.C_Charge_ID >0 "); //Hanlded case: order is not selected and on the change/selection
-                                                                                                                                                                                                                                //of deliverydate excception's coming  missing expression
+                //Hanlded case: order is not selected and on the change/selection of deliverydate excception's coming  missing expression                                                                                                                                            
+                sql.Append(" LEFT OUTER JOIN M_AttributeSetInstance ins ON (ins.M_AttributeSetInstance_ID =l.M_AttributeSetInstance_ID) WHERE l.C_Order_ID=" + (C_Ord_IDs == null ? 0 : C_Ord_IDs) + " AND C.C_Charge_ID >0 "); 
 
                 if (DelivDates != "")
                 {
