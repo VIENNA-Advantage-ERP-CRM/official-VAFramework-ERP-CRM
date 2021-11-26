53b6043be02f30496188af1ad8d18f0764ef7ed4
VALokeshChauhan <lokesh.chauhan@viennasolutions.com>
2021-11-26 19:17:54 +0530

    update changes for data only message

--- VIS/Areas/VIS/Controllers/HomeController.cs
+++ VIS/Areas/VIS/Controllers/HomeController.cs
@@ -346,7 +346,7 @@ namespace VIS.Controllers
 
                     /// VIS0008
                     /// Check applied for adding message to toastr if 2FA method is VA and VA App is not linked with device
-                    if (!LoginHelper.IsDeviceLinked(AD_User_ID))
+                    if (!LoginHelper.IsDeviceLinked(ctx, AD_User_ID))
                         ModelLibrary.PushNotif.SSEManager.Get().AddMessage(ctx.GetAD_Session_ID(), Msg.GetMsg(ctx, "PlzLinkVAApp"));
                 }
             }
