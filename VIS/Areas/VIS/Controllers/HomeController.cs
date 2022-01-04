ae1ccb9820b96d952aef7659c8185372e7a0729c
VALokeshChauhan <lokesh.chauhan@viennasolutions.com>
2022-01-04 12:00:34 +0530

    Change to initiate thread at login

--- VIS/Areas/VIS/Controllers/HomeController.cs
+++ VIS/Areas/VIS/Controllers/HomeController.cs
@@ -348,6 +348,8 @@ namespace VIS.Controllers
                     /// Check applied for adding message to toastr if 2FA method is VA and VA App is not linked with device
                     if (!LoginHelper.IsDeviceLinked(ctx, AD_User_ID))
                         ModelLibrary.PushNotif.SSEManager.Get().AddMessage(ctx.GetAD_Session_ID(), Msg.GetMsg(ctx, "PlzLinkVAApp"));
+
+                    VAdvantage.Classes.ThreadInstance.Get().Start();
                 }
             }
 
