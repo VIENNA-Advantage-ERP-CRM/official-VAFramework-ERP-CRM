57c4e11b7142b46565d37250734752017f6cba90
Karan Malli <karan@viennasolutions.com>
2022-01-05 10:12:06 +0530

    Queries in log fixed.

--- VIS/Areas/VIS/Helpers/HomeHelper.cs
+++ VIS/Areas/VIS/Helpers/HomeHelper.cs
@@ -165,8 +165,8 @@ namespace VIS.Helpers
                          .Append(" Join ad_user Au on au.ad_user_id= CH.createdBy")
                          .Append(" left outer JOIN ad_image AI on(ai.ad_image_id=au.ad_image_id)")
                          .Append("  join ad_window AW on(cs.ad_window_id= aw.ad_window_id) left outer  JOIN ad_image adi on(adi.ad_image_id= aw.ad_image_id)  where cs.createdby=" + ctx.GetAD_User_ID())
-                         .Append(" and ch.cm_chatentry_ID =(Select max(cm_chatentry_ID) from cm_chatentry where CM_Chat_ID= ch.cm_chat_id)")
-                         .Append("  order by inn.EntryID desc,ch.cm_chatentry_id asc");
+                         .Append(" and ch.cm_chatentry_ID =(Select max(cm_chatentry_ID) from cm_chatentry where CM_Chat_ID= ch.cm_chat_id)");
+                       // .Append("  order by inn.EntryID desc,ch.cm_chatentry_id asc");
                 dsData = new DataSet();
                 dsData = DB.ExecuteDataset(SqlQuery.ToString());
                 int nTotalFollow = 0;
