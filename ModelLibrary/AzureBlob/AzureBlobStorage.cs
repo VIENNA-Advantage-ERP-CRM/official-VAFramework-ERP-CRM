ccac30275c7a82c277e73849fd773a48071dde8d
amir-adeli <amir.adelipour@viennaadvantage.com>
2022-02-16 15:45:47 +0530

    Added Upload and Download functionality

--- ModelLibrary/AzureBlob/AzureBlobStorage.cs
+++ ModelLibrary/AzureBlob/AzureBlobStorage.cs
@@ -14,65 +14,67 @@ namespace VAdvantage.AzureBlob
     {
         private static VLogger _log = VLogger.GetVLogger(typeof(AzureBlobStorage).FullName);
 
-        public static string UploadFile(string fullFilePath)
+        /// <summary>
+        /// Upload the specified file to the specified Azure blob container
+        /// </summary>
+        /// <param name="containerUrl"></param>
+        /// <param name="fullFilePath"></param>
+        /// <returns>true if succeeded, false if failed</returns>
+        public static string UploadFile(Ctx ctx, string containerUrl, string fullFilePath)
         {
-            //if (!Env.IsModuleInstalled("VA090_"))
-            //{
-            //    _log.SaveError("AzureBlob", "Module VA090_AzureBlobStorage is not installed!");
-            //    return "Module VA090_AzureBlobStorage is not installed!";
-            //}
+            if (!Env.IsModuleInstalled("VA090_"))
+            {
+                _log.SaveError("AzureBlob", "Module VA090_AzureBlobStorage is not installed!");
+                return Msg.GetMsg(ctx, "VIS_VA090NotInstalled");
+            }
 
-            //try
-            //{
             Assembly assembly = Assembly.Load("VA090Svc");
             Type type = assembly.GetType("VA090Svc.Classes.AzureBlobStorage");
 
-            //if (type != null)
-            //{
-            object[] param = new object[1];
-            param[0] = fullFilePath;
-            var resultObj = type.GetMethod("UploadFile").Invoke(null, param);
-            Exception result = (Exception)resultObj;
-            return "";
-            //    }
-            //    return "Type is null";
-            //}
-            //catch (Exception ex)
-            //{
-            //    _log.SaveError("AzureBlob", "Exception: " + ex.Message);
-            //    return ex.Message;
-            //}
-        }
-        public static string DownloadFile(string containerUrl, string downloadFullPath, string fileName)
-        {
-            //if (!Env.IsModuleInstalled("VA090_"))
-            //{
-            //    _log.SaveError("AzureBlob", "Module VA090_AzureBlobStorage is not installed!");
-            //    return "Module VA090_AzureBlobStorage is not installed!";
-            //}
-
-            //try
-            //{
-            Assembly assembly = Assembly.Load("VA090Svc");
-            Type type = assembly.GetType("VA090Svc.Classes.AzureBlobStorage");
-
-            //if (type != null)
-            //{
             object[] param = new object[3];
-            param[0] = containerUrl;
-            param[1] = downloadFullPath;
-            param[2] = fileName;
+            param[0] = ctx;
+            param[1] = containerUrl;
+            param[2] = fullFilePath;
+            var resultObj = type.GetMethod("UploadFile").Invoke(null, param);
+
+            if (resultObj != null)
+            {
+                return resultObj.ToString();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Download the specified file to the specified Azure blob container
+        /// </summary>
+        /// <param name="containerUrl"></param>
+        /// <param name="downloadFullPath"></param>
+        /// <param name="fileName"></param>
+        /// <returns>true if succeeded, false if failed</returns>
+        public static string DownloadFile(Ctx ctx, string containerUrl, string downloadFullPath, string fileName)
+        {
+
+            if (!Env.IsModuleInstalled("VA090_"))
+            {
+                _log.SaveError("AzureBlob", "Module VA090_AzureBlobStorage is not installed!");
+                return Msg.GetMsg(ctx, "VIS_VA090NotInstalled");
+            }
+
+            Assembly assembly = Assembly.Load("VA090Svc");
+            Type type = assembly.GetType("VA090Svc.Classes.AzureBlobStorage");
+
+            object[] param = new object[4];
+            param[0] = ctx;
+            param[1] = containerUrl;
+            param[2] = downloadFullPath;
+            param[3] = fileName;
             var resultObj = type.GetMethod("DownloadFile").Invoke(null, param);
-            Exception result = (Exception)resultObj;
-            return "";
-            //    }
-            //    return "Type is null";
-            //}
-            //catch (Exception ex)
-            //{
-            //    _log.SaveError("AzureBlob", "Exception: " + ex.Message);
-            //    return ex.Message;
-            //}
+
+            if (resultObj != null)
+            {
+                return resultObj.ToString();
+            }
+            return null;
         }
     }
 }
