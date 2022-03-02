c25978c6269e4f4df8f296715e7e7a6ed0557930
Bharat Singla <bharat.singla@viennasolutions.com>
2022-03-02 13:16:48 +0530

    Changes done on callout on Order Line for code optimization

--- VIS/Areas/VIS/Models/Callouts/MPriceListModel.cs
+++ VIS/Areas/VIS/Models/Callouts/MPriceListModel.cs
@@ -18,7 +18,7 @@ namespace VIS.Models
         /// <param name="ctx"></param>
         /// <param name="fields"></param>
         /// <returns></returns>
-        public Dictionary<String, String> GetPriceList(Ctx ctx,string fields)
+        public Dictionary<String, String> GetPriceList(Ctx ctx, string fields)
         {
             if (fields != null)
             {
@@ -36,6 +36,8 @@ namespace VIS.Models
                 retDic["PriceListPrecision"] = prcLst.GetPricePrecision().ToString();
                 //JID_1744  Precision should be as per currency percision 
                 retDic["StdPrecision"] = crncy.GetStdPrecision().ToString();
+                retDic["EnforcePriceLimit"] = prcLst.IsEnforcePriceLimit() ? "Y" : "N";
+                retDic["IsTaxIncluded"] = prcLst.IsTaxIncluded() ? "Y" : "N";
                 return retDic;
             }
             else
@@ -51,29 +53,29 @@ namespace VIS.Models
         /// <param name="fields">Parameters</param>
         /// <returns>List of Data</returns>
         public Dictionary<String, Object> GetPriceListData(Ctx ctx, string fields)
-        {            
-                int M_PriceList_ID;                
-                M_PriceList_ID = Util.GetValueOfInt(fields);
-                Dictionary<String, Object> retDic = null;
-                string sql = "SELECT pl.IsTaxIncluded,pl.EnforcePriceLimit,pl.C_Currency_ID,c.StdPrecision,"
-                + "plv.M_PriceList_Version_ID,plv.ValidFrom "
-                + "FROM M_PriceList pl,C_Currency c,M_PriceList_Version plv "
-                + "WHERE pl.C_Currency_ID=c.C_Currency_ID"
-                + " AND pl.M_PriceList_ID=plv.M_PriceList_ID"
-                + " AND pl.M_PriceList_ID=" + M_PriceList_ID						//	1
-                + "ORDER BY plv.ValidFrom DESC";
-                DataSet ds = DB.ExecuteDataset(sql, null, null);
-                if (ds != null && ds.Tables[0].Rows.Count > 0)
-                {
-                    retDic = new Dictionary<String, Object>();
-                    retDic["IsTaxIncluded"] = Util.GetValueOfString(ds.Tables[0].Rows[0]["IsTaxIncluded"]);
-                    retDic["EnforcePriceLimit"] = Util.GetValueOfString(ds.Tables[0].Rows[0]["EnforcePriceLimit"]);
-                    retDic["StdPrecision"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["StdPrecision"]);
-                    retDic["C_Currency_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["C_Currency_ID"]);
-                    retDic["M_PriceList_Version_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["M_PriceList_Version_ID"]);                    
-                    retDic["ValidFrom"] = Util.GetValueOfDateTime(ds.Tables[0].Rows[0]["ValidFrom"]);
-                }
-                return retDic;
+        {
+            int M_PriceList_ID;
+            M_PriceList_ID = Util.GetValueOfInt(fields);
+            Dictionary<String, Object> retDic = null;
+            string sql = "SELECT pl.IsTaxIncluded,pl.EnforcePriceLimit,pl.C_Currency_ID,c.StdPrecision,"
+            + "plv.M_PriceList_Version_ID,plv.ValidFrom "
+            + "FROM M_PriceList pl,C_Currency c,M_PriceList_Version plv "
+            + "WHERE pl.C_Currency_ID=c.C_Currency_ID"
+            + " AND pl.M_PriceList_ID=plv.M_PriceList_ID"
+            + " AND pl.M_PriceList_ID=" + M_PriceList_ID                        //	1
+            + "ORDER BY plv.ValidFrom DESC";
+            DataSet ds = DB.ExecuteDataset(sql, null, null);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                retDic = new Dictionary<String, Object>();
+                retDic["IsTaxIncluded"] = Util.GetValueOfString(ds.Tables[0].Rows[0]["IsTaxIncluded"]);
+                retDic["EnforcePriceLimit"] = Util.GetValueOfString(ds.Tables[0].Rows[0]["EnforcePriceLimit"]);
+                retDic["StdPrecision"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["StdPrecision"]);
+                retDic["C_Currency_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["C_Currency_ID"]);
+                retDic["M_PriceList_Version_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["M_PriceList_Version_ID"]);
+                retDic["ValidFrom"] = Util.GetValueOfDateTime(ds.Tables[0].Rows[0]["ValidFrom"]);
+            }
+            return retDic;
         }
 
         /// <summary>
@@ -93,7 +95,7 @@ namespace VIS.Models
             + "FROM M_PriceList pl,C_Currency c,M_PriceList_Version plv "
             + "WHERE pl.C_Currency_ID=c.C_Currency_ID"
             + " AND pl.M_PriceList_ID=plv.M_PriceList_ID"
-            + " AND pl.M_PriceList_ID=" + M_PriceList_ID                       
+            + " AND pl.M_PriceList_ID=" + M_PriceList_ID
             + "ORDER BY plv.ValidFrom DESC";
             DataSet ds = DB.ExecuteDataset(sql, null, null);
             if (ds != null && ds.Tables[0].Rows.Count > 0)
