14e778cccad2dba9a03726d91fec7d5fdb7191e3
liveserver <live@viennasolutions.com>
2022-03-04 05:17:04 +0000

    1.0.63.0

--- ModelLibrary/Properties/AssemblyInfo.cs
+++ ModelLibrary/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
 
-[assembly: AssemblyFileVersion("1.9.3.0")] 
+[assembly: AssemblyFileVersion("1.9.4.0")] 
 
