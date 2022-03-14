c40bdb8263dcc6f6b74b0e6ccbcffb5a432a4605
Amit Bansal <amit.bansal@viennasolutions.com>
2022-03-14 14:03:43 +0530

    1. handle Query Log 2. change transfer Org Ownership for Warehous, Product and Business Partner window tabs

--- ModelLibrary/Process/OrgOwnership.cs
+++ ModelLibrary/Process/OrgOwnership.cs
@@ -22,7 +22,8 @@ using VAdvantage.Utility;
 using System.Data;
 using VAdvantage.Logging;
 
-using VAdvantage.ProcessEngine;namespace VAdvantage.Process
+using VAdvantage.ProcessEngine;
+namespace VAdvantage.Process
 {
     public class OrgOwnership : ProcessEngine.SvrProcess
     {
@@ -33,6 +34,7 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
         private int _M_Product_ID = -1;
         private int _C_BP_Group_ID = -1;
         private int _C_BPartner_ID = -1;
+        private MSession session = null;
 
         /// <summary>
         /// Prepare - e.g., get Parameters.
@@ -93,6 +95,8 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
                 throw new Exception("OrgOwnership - invalid AD_Org_ID=" + _AD_Org_ID);
             }
 
+            session = MSession.Get(GetCtx());
+
             GeneralOwnership();
 
             if (_M_Warehouse_ID > 0)
@@ -134,16 +138,24 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
                 .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
             int no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Warehouse_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Warehouse.Table_ID, sql.ToString(), 1);
+            }
 
-            //	Set Accounts
-            sql = new StringBuilder();
-            sql.Append("UPDATE M_Warehouse_Acct "
-                + "SET AD_Org_ID=").Append(_AD_Org_ID)
-                .Append(" WHERE M_Warehouse_ID=").Append(_M_Warehouse_ID)
-                .Append(" AND AD_Client_ID=").Append(GetAD_Client_ID())
-                .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
-            no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
-            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            //	VIS_0045: 11-03-22 -> Set Accounts
+            if (Env.IsModuleInstalled("FRPT_"))
+            {
+                sql = new StringBuilder();
+                sql.Append("UPDATE FRPT_Warehouse_Acct "
+                    + "SET AD_Org_ID=").Append(_AD_Org_ID)
+                    .Append(" WHERE M_Warehouse_ID=").Append(_M_Warehouse_ID)
+                    .Append(" AND AD_Client_ID=").Append(GetAD_Client_ID())
+                    .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
+                no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            }
 
             //	Set Locators
             sql = new StringBuilder();
@@ -154,6 +166,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
                 .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
             no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Locator_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Locator.Table_ID, sql.ToString(), 1);
+            }
 
             //	Set Storage
             sql = new StringBuilder();
@@ -166,6 +183,28 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
                 .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
             no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Storage"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Storage.Table_ID, sql.ToString(), 1);
+            }
+
+            //	VIS_0045: 11-03-22 -> Set Container Storage
+            sql = new StringBuilder();
+            sql.Append("UPDATE M_ContainerStorage s "
+                + "SET AD_Org_ID=").Append(_AD_Org_ID)
+                .Append(" WHERE EXISTS "
+                    + "(SELECT * FROM M_Locator l WHERE l.M_Locator_ID=s.M_Locator_ID"
+                    + " AND l.M_Warehouse_ID=").Append(_M_Warehouse_ID)
+                .Append(") AND AD_Client_ID=").Append(GetAD_Client_ID())
+                .Append(" AND AD_Org_ID<>").Append(_AD_Org_ID);
+            no = DataBase.DB.ExecuteQuery(sql.ToString(), null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ContainerStorage_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ContainerStorage.Table_ID, sql.ToString(), 1);
+            }
             return "";
         }
 
@@ -196,11 +235,19 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             String sql = "UPDATE M_Product x " + set;
             int no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Product_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Product.Table_ID, sql, 1);
+            }
 
-            //	Acct
-            sql = "UPDATE M_Product_Acct x " + set;
-            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
-            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            //	VIS_0045: 11-03-22 -> Acct
+            if (Env.IsModuleInstalled("FRPT_"))
+            {
+                sql = "UPDATE FRPT_Product_Acct x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            }
 
             //	BOM
             sql = "UPDATE M_Product_BOM x " + set;
@@ -211,28 +258,145 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             sql = "UPDATE M_Product_PO x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Product_PO"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Product_PO.Table_ID, sql, 1);
+            }
 
             //	Trl
             sql = "UPDATE M_Product_Trl x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "AD_Language"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Product_Trl.Table_ID, sql, 1);
+            }
 
             //Added by Pratap 30-12-15 M_Replenish,M_Substitute,C_BPartner_Product - Mantis Issue ID - 0000441
             //	M_Replenish 
             sql = "UPDATE M_Replenish x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Replenish"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Replenish.Table_ID, sql, 1);
+            }
 
             //	M_Substitute
             sql = "UPDATE M_Substitute x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Substitute"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Substitute.Table_ID, sql, 1);
+            }
 
             //	C_BPartner_Product
             sql = "UPDATE C_BPartner_Product x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_BPartner_Product"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BPartner_Product.Table_ID, sql, 1);
+            }
 
+            // VIS_0045: 11-03-22
+            if (Env.IsModuleInstalled("VA076_"))
+            {
+                sql = "UPDATE VA076_Attributes x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "VA076_Attributes_ID"));
+
+                sql = "UPDATE VA076_Attributes2  x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "VA076_Attributes2_ID"));
+            }
+
+            sql = "UPDATE M_RelatedProduct  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_RelatedProduct_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_RelatedProduct.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_ProductLocator  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ProductLocator_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ProductLocator.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_ProductDownload   x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ProductDownload_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ProductDownload.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE C_UOM_Conversion   x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_UOM_Conversion_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_UOM_Conversion.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_ProductAttributes   x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ProductAttributes_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ProductAttributes.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_Attribute  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_Attribute_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Attribute.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_ProductAttributeImage  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ProductAttributeImage_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ProductAttributeImage.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_TransactionSummary  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_TransactionSummary_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_TransactionSummary.Table_ID, sql, 1);
+            }
+
+            sql = "UPDATE M_ProductStockSummary  x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "M_ProductStockSummary_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_ProductStockSummary.Table_ID, sql, 1);
+            }
 
             return "";
         }
@@ -263,31 +427,65 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             int no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_BPartner_ID"));
 
-            //	Acct xxx
-            sql = "UPDATE C_BP_Customer_Acct x " + set;
-            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
-            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
-            sql = "UPDATE C_BP_Employee_Acct x " + set;
-            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
-            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
-            sql = "UPDATE C_BP_Vendor_Acct x " + set;
-            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
-            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BPartner.Table_ID, sql, 1);
+            }
+
+            // VIS_0045: 11-03-22 -> Acct xxx
+            if (Env.IsModuleInstalled("FRPT_"))
+            {
+                sql = "UPDATE FRPT_BP_Customer_Acct x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+                sql = "UPDATE FRPT_BP_Employee_Acct x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+                sql = "UPDATE FRPT_BP_Vendor_Acct x " + set;
+                no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+                AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_AcctSchema_ID"));
+            }
 
             //	Location
             sql = "UPDATE C_BPartner_Location x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_BPartner_Location_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BPartner_Location.Table_ID, sql, 1);
+            }
 
             //	Contcat/User
             sql = "UPDATE AD_User x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "AD_User_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_AD_User.Table_ID, sql, 1);
+            }
 
             //	BankAcct
             sql = "UPDATE C_BP_BankAccount x " + set;
             no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "C_BP_BankAccount_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BP_BankAccount.Table_ID, sql, 1);
+            }
+
+            // VIS_0045: 11-03-22 -> BP Access
+            sql = "UPDATE AD_UserBPAccess x " + set;
+            no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
+            AddLog(0, null, new Decimal(no), Msg.Translate(GetCtx(), "AD_UserBPAccess_ID"));
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_AD_UserBPAccess.Table_ID, sql, 1);
+            }
 
             return "";
         }
@@ -306,7 +504,12 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             int no = DataBase.DB.ExecuteQuery(sql, null, Get_TrxName());
             if (no != 0)
             {
-                log.Fine("generalOwnership - R_ContactInterest=" + no);
+                log.Fine("generalOwnership - R_ContactInterest=" + no);                
+            }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_R_ContactInterest.Table_ID, sql.ToString(), 1);
             }
 
             //	AD_User_Roles
@@ -316,6 +519,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             {
                 log.Fine("generalOwnership - AD_User_Roles=" + no);
             }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_AD_User_Roles.Table_ID, sql.ToString(), 1);
+            }
 
             //	C_BPartner_Product
             sql = "UPDATE C_BPartner_Product " + set;
@@ -323,6 +531,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             if (no != 0)
             {
                 log.Fine("generalOwnership - C_BPartner_Product=" + no);
+            }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BPartner_Product.Table_ID, sql.ToString(), 1);
             }
 
             //	Withholding
@@ -332,6 +545,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             {
                 log.Fine("generalOwnership - C_BP_Withholding=" + no);
             }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_C_BP_Withholding.Table_ID, sql.ToString(), 1);
+            }
 
             //	Costing
             sql = "UPDATE M_Product_Costing " + set;
@@ -340,6 +558,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             {
                 log.Fine("generalOwnership - M_Product_Costing=" + no);
             }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Product_Costing.Table_ID, sql.ToString(), 1);
+            }
 
             //	Replenish
             sql = "UPDATE M_Replenish " + set;
@@ -347,6 +570,11 @@ using VAdvantage.ProcessEngine;namespace VAdvantage.Process
             if (no != 0)
             {
                 log.Fine("generalOwnership - M_Replenish=" + no);
+            }
+            // maintain query log
+            if (no > 0)
+            {
+                session.QueryLog(GetAD_Client_ID(), GetAD_Org_ID(), X_M_Replenish.Table_ID, sql.ToString(), 1);
             }
 
         }
