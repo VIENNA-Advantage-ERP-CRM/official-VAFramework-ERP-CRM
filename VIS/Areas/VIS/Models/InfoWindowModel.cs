1b0c3333f13786d87514e98a1c0d8cd1d25d5f12
Karan Malli <karan@viennasolutions.com>
2022-06-15 14:16:09 +0530

    Almost Donr info window

--- VIS/Areas/VIS/Models/InfoWindowModel.cs
+++ VIS/Areas/VIS/Models/InfoWindowModel.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using VAdvantage.Classes;
+using VAdvantage.DataBase;
 using VAdvantage.Model;
 using VAdvantage.Utility;
 using VIS.DBase;
@@ -348,8 +349,370 @@ namespace VIS.Models
 
 
 
-        public InfoData GetData(string sql, string tableName, int pageNo, VAdvantage.Utility.Ctx ctx)
+        public InfoData GetData(string tableName, int pageNo, VAdvantage.Utility.Ctx ctx, string selectedIDs
+            , bool requery, Info info, string validationCode, List<InfoSearchCol> srchCtrls)
         {
+
+            string sql = "SELECT ";
+            string colName = null;
+            string tabname = null;
+            string keyCol = null;
+            string cName = null;
+            int displayType = 0;
+            List<InfoSchema> schema = info.Schema;
+            if (schema != null)
+            { 
+            //get Qry from InfoColumns
+            for (int item = 0; item < schema.Count; item++)
+            {
+                colName = schema[item].SelectClause;
+                if (colName.IndexOf('.') > -1)
+                {
+                    cName = colName.Substring(colName.LastIndexOf('.') + 1);
+                }
+                else
+                {
+                    cName = colName;
+                }
+
+                if (schema[item].IsKey)
+                {
+                    keyCol = cName.ToUpper();
+                    tabname = keyCol.Substring(0, keyCol.IndexOf("_ID"));
+                }
+                displayType = schema[item].AD_Reference_ID;
+                if (displayType == DisplayType.YesNo)
+                {
+                    sql += " ( CASE " + colName + " WHEN 'Y' THEN  'True' ELSE 'False'  END ) AS " + (cName);
+                }
+                else if (displayType == DisplayType.List)
+                {
+                    //    ValueNamePair[] values = MRefList.GetList(Util.GetValueOfInt(ds.Tables[0].Rows[i]["AD_REFERENCE_VALUE_ID"]), false);
+                    //    sql.Append(" CASE ");
+                    //    for (int j = 0; j < values.Length; j++)
+                    //    {
+                    //        sql.Append(" WHEN " + colName + "='" + values[j].Key.ToString() + "' THEN '" + values[j].Name.ToString() + "'");
+                    //    }
+                    //    sql.Append(" END AS " + cName);
+                    var refList = schema[item].RefList;
+                    sql += (" CASE ");
+                    foreach (var refListItem in refList)
+                    {
+                        sql += " WHEN " + colName + "='" + refListItem.Key + "' THEN '" + refListItem.Value + "'";
+                    }
+                    sql += " END AS " + cName;
+                    //sql += colName + " ";
+                }
+                else
+                {
+                    sql += colName + " ";
+                }
+                if (!((schema.Count - 1) == item))
+                {
+                    sql += ", ";
+                }
+
+            }
+        }
+
+            if (selectedIDs != null && selectedIDs.Length > 0)
+            {
+                sql += ", 'N' AS ordcol";
+            }
+
+
+            // sql=sql.substring(0,sql.Length-2)
+            sql += " FROM " + info.FromClause;
+            string sqlOrderby = "";
+            if (requery == true)
+            {
+                //Get Where Clause From SearchControls
+                //  debugger;
+                var whereClause = " ";
+                string srchValue = null;
+                var appendAND = false;
+                for (var i = 0; i < srchCtrls.Count; i++)
+                {
+                    srchValue = Convert.ToString(srchCtrls[i].Value);
+
+                    //JID_0905:  In Case of Date Range, if From Date is not selected then check if To Date is selected
+                    if (srchCtrls[i].AD_Reference_ID == DisplayType.Date && srchCtrls[i].IsRange)
+                    {
+                        if (srchValue == null)
+                        {
+                            srchValue = Convert.ToString(srchCtrls[i].Value);
+                        }
+                    }
+
+                    // Consider checkbox value only in case of true value
+                    if (srchValue == null || srchValue.Length == 0 || (srchValue == "0" && srchCtrls[i].AD_Reference_ID != DisplayType.YesNo) || srchValue == "-1")
+                    {
+                        continue;
+                    }
+
+                    {
+                        if (appendAND == true)
+                        {
+                            whereClause += " AND ";
+                        }
+                        if (srchCtrls[i].AD_Reference_ID == DisplayType.String
+                            || srchCtrls[i].AD_Reference_ID == DisplayType.Text
+                            || srchCtrls[i].AD_Reference_ID == DisplayType.TextLong)
+                        {
+
+
+                            if (!((srchValue).IndexOf("%") == 0))
+                            {
+                                srchValue = "●" + srchValue;
+                            }
+                            else
+                            {
+                                srchValue = (srchValue).Replace("%", "●");
+                            }
+                            if (!(((srchValue).LastIndexOf("●")) == ((srchValue).Length)))
+                            {
+                                srchValue = srchValue + "●";
+                            }
+                            srchValue = GlobalVariable.TO_STRING(srchValue);
+                            whereClause += "  UPPER(" + srchCtrls[i].SearchColumnName + ") LIKE " + srchValue.ToUpper();
+                        }
+                        else if (srchCtrls[i].AD_Reference_ID == DisplayType.Date)
+                        {
+                            string fromValue = null;
+                            string toValue = null;
+                            DateTime date = Convert.ToDateTime((srchCtrls[i].Value));
+                            fromValue = "TO_DATE( '" + ((date.Month) + 1) + "-" + date.Day + "-" + date.Year + "', 'MM-DD-YYYY')";// GlobalVariable.TO_DATE(Util.GetValueOfDateTime(srchCtrls[i].Ctrl.getValue()), true);
+
+                            if (srchCtrls[i].IsRange)
+                            {
+                                // JID_0905: If To Date is empty, then select system date
+                                if (srchCtrls[i].Value == null)
+                                {
+                                    date = DateTime.Now;
+                                }
+                                else
+                                {
+                                    date = Convert.ToDateTime((srchCtrls[i].ValueTo));
+                                }
+                                toValue = "TO_DATE( '" + ((date.Month) + 1) + "-" + date.Day + "-" + date.Year + "', 'MM-DD-YYYY')";// GlobalVariable.TO_DATE(Util.GetValueOfDateTime(srchCtrls[i].Ctrl.getValue()), true);
+                                whereClause += " ( " + srchCtrls[i].SearchColumnName + " BETWEEN " + fromValue + " AND " + toValue + ")";
+                            }
+                            else
+                            {
+                                whereClause += srchCtrls[i].SearchColumnName + " =" + fromValue;
+                            }
+                        }
+                        else if (srchCtrls[i].AD_Reference_ID == DisplayType.DateTime)
+                        {
+                            string fromValue = null;
+                            string toValue = null;
+                            var date = Convert.ToDateTime((srchCtrls[i].Value));
+                            fromValue = "TO_DATE( '" + ((date.Month) + 1) + "-" + date.Day + "-" + date.Year + " " + date.Hour + ":" + date.Minute + ":" + date.Second + "', 'MM-DD-YYYY HH24:MI:SS')";// GlobalVariable.TO_DATE(Util.GetValueOfDateTime(srchCtrls[i].Ctrl.getValue()), true);
+
+                            if (srchCtrls[i].IsRange)
+                            {
+                                date = Convert.ToDateTime((srchCtrls[i].ValueTo));
+                                toValue = "TO_DATE( '" + ((date.Month) + 1) + "-" + date.Day + "-" + date.Year + " " + date.Hour + ":" + date.Minute + ":" + date.Second + "', 'MM-DD-YYYY HH24:MI:SS')";// GlobalVariable.TO_DATE(Util.GetValueOfDateTime(srchCtrls[i].Ctrl.getValue()), true);
+                                whereClause += " ( " + srchCtrls[i].SearchColumnName + " BETWEEN " + fromValue + " AND " + toValue + ")";
+                            }
+                            else
+                            {
+                                whereClause += srchCtrls[i].SearchColumnName + " =" + fromValue;
+                            }
+                        }
+
+                        else if (srchCtrls[i].AD_Reference_ID == DisplayType.YesNo)
+                        {
+
+
+                            srchValue = Convert.ToBoolean(srchCtrls[i].Value) == true ? "Y" : "N";
+                            whereClause += srchCtrls[i].SearchColumnName + " = '" + srchValue + "'";
+
+                        }
+                        else
+                        {
+                            string fromValue = null;
+                            string toValue = null;
+                            fromValue = Convert.ToString(srchCtrls[i].Value);
+                            if (srchCtrls[i].IsRange)
+                            {
+                                //if (srchCtrls[i].Value)
+                                //{     // Consider checkbox value only in case of true value
+                                    srchValue = Convert.ToBoolean(srchCtrls[i].Value) == true ? "Y" : "";
+                                    whereClause += srchCtrls[i].SearchColumnName + " = '" + srchValue + "'";
+                                //}
+                            }
+                            else
+                            {
+                                whereClause += " " + srchCtrls[i].SearchColumnName + " ='" + fromValue + "'";
+                            }
+
+
+                        }
+
+
+
+
+                        appendAND = true;
+
+
+
+                    }
+
+                }
+
+                // VIS0060: Handled case of alise name on Info window.
+                sql = MRole.GetDefault(ctx).AddAccessSQL(sql, info.TableName, true, false);
+
+                if (whereClause.Length > 1)
+                {
+                    if (info.FromClause.ToUpper().IndexOf("WHERE") > -1 || sql.ToUpper().IndexOf("WHERE") > -1)
+                    {
+                        sql += " AND " + whereClause;
+                    }
+                    else
+                    {
+                        sql += " WHERE " + whereClause;
+                    }
+                    if (validationCode != null && validationCode.Length > 0)
+                    {
+                        sql += " AND " + validationCode;
+                    }
+                }
+                else if (validationCode != null && validationCode.Length > 0)
+                {
+                    if (info.FromClause != null)
+                    {
+                        if (info.FromClause.ToUpper().IndexOf("WHERE") > -1 || sql.ToUpper().IndexOf("WHERE") > -1)
+                        {
+                            sql += " AND " + validationCode;
+                        }
+                        else
+                        {
+                            sql += " WHERE " + validationCode;
+                        }
+                    }
+                }
+
+
+
+
+
+
+
+                if (info.OTHERCLAUSE != null)
+                {
+                    if (selectedIDs != null && selectedIDs.Length > 0)
+                    {
+                        var otherClause = "";
+                        if (info.OTHERCLAUSE.IndexOf("ORDER BY") > -1)
+                        {
+                            otherClause = info.OTHERCLAUSE.Substring(0, info.OTHERCLAUSE.IndexOf("ORDER BY")); ;
+                            sqlOrderby = info.OTHERCLAUSE.Substring(info.OTHERCLAUSE.IndexOf("ORDER BY"));
+                        }
+                        if (otherClause != null && otherClause.Length > 0)
+
+                            if ((otherClause).ToUpper().IndexOf("WHERE") > -1)
+                            {
+                                if (info.FromClause.ToUpper().IndexOf("WHERE") > -1
+                                    || whereClause.Length > 1
+                                    || (validationCode != null && validationCode.Length > 1))
+                                {
+                                    otherClause = (otherClause).Replace("WHERE", "AND");
+                                }
+
+                            }
+                        sql += " " + otherClause;
+                    }
+                    else
+                    {
+                        if ((info.OTHERCLAUSE).ToUpper().IndexOf("WHERE") > -1)
+                        {
+                            if (info.FromClause.ToUpper().IndexOf("WHERE") > -1
+                                || whereClause.Length > 1
+                                || (validationCode != null && validationCode.Length > 1))
+                            {
+                                info.OTHERCLAUSE = (info.OTHERCLAUSE).Replace("WHERE", "AND");
+                            }
+
+                        }
+                        sql += " " + info.OTHERCLAUSE;
+                    }
+                }
+            }
+            else
+            {
+                if (info.FromClause.ToUpper().IndexOf("WHERE") > -1)
+                {
+                    sql += " AND rownum=-1";
+                }
+                else
+                {
+                    sql += " WHERE rownum=-1";
+                }
+
+            }
+
+
+
+            if (selectedIDs != null && selectedIDs.Length > 0)
+            {
+                var sqlUnion = " UNION " + sql;
+                sqlUnion = sqlUnion.Replace("'N' AS ordcol", "'Y' AS ordcol");
+
+                if (sql.ToUpper().IndexOf("WHERE") > -1)
+                {
+                    sql += " AND " + tabname + "." + keyCol + " IN(" + selectedIDs + ")";
+                    sql += sqlUnion;
+                    sql += " AND " + tabname + "." + keyCol + " NOT IN(" + selectedIDs + ")";
+                }
+                //else {
+                //    sql += " WHERE " + tabname + "." + keyCol + " IN(" + selectedIDs + ")";
+                //    sql += sqlUnion;
+                //    sql += " AND " + tabname + "." + keyCol + " NOT IN(" + selectedIDs + ")";
+                //}
+                sql = "SELECT * FROM (" + sql + ") tblquery";
+            }
+
+
+            if (selectedIDs != null && selectedIDs.Length > 0)
+            {
+                if (sqlOrderby.ToUpper().IndexOf("ORDER BY") > -1)
+                {
+                    sqlOrderby = sqlOrderby.ToUpper().Replace("ORDER BY", "ORDER BY ordcol ASC,");
+
+
+                    //sql = sql.substr(0, sql.IndexOf("ORDER BY"));
+                    string[] sqlOrderbyArr = sqlOrderby.Split(',');
+                    var finalorderBy = " ORDER BY ";
+                    for (var l = 0; l < sqlOrderbyArr.Length; l++)
+                    {
+                        sqlOrderbyArr[l] = sqlOrderbyArr[l].Replace("ORDER BY ", "");
+                        if (sqlOrderbyArr[l].IndexOf(".") > -1)
+                        {
+                            sqlOrderbyArr[l] = sqlOrderbyArr[l].Replace(sqlOrderbyArr[l].Substring(0, sqlOrderbyArr[l].IndexOf(".") + 1), "tblquery.");
+                        }
+                        else
+                        {
+                            sqlOrderbyArr[l] = "tblquery." + sqlOrderbyArr[l];
+                        }
+                            
+                            if (l > 0)
+                            finalorderBy += ",";
+                        finalorderBy += sqlOrderbyArr[l];
+                    }
+                    sql += finalorderBy;
+                }
+                else
+                {
+                    sql += " ORDER BY ordcol ASC";
+                }
+
+
+
+            }
+
+
 
             InfoData _iData = new InfoData();
             try
@@ -360,7 +723,7 @@ namespace VIS.Models
                 //               MRole.SQL_FULLYQUALIFIED, MRole.SQL_RO);
 
 
-                int totalRec = Util.GetValueOfInt(DB.ExecuteScalar("SELECT COUNT(*) FROM ( " + sql + " ) t", null, null));
+                int totalRec = Util.GetValueOfInt(VAdvantage.DataBase.DB.ExecuteScalar("SELECT COUNT(*) FROM ( " + sql + " ) t", null, null));
                 int pageSize = 50;
                 PageSetting pSetting = new PageSetting();
                 pSetting.CurrentPage = pageNo;
@@ -431,7 +794,7 @@ namespace VIS.Models
         /// <returns></returns> info window id.
         public int GetInfoWindowID(string InfoSearchKey)
         {
-            return Util.GetValueOfInt(DB.ExecuteScalar("SELECT AD_InfoWindow_ID FROM AD_InfoWindow WHERE IsActive='Y' AND Value='" + InfoSearchKey + "'", null, null));
+            return Util.GetValueOfInt(VAdvantage.DataBase.DB.ExecuteScalar("SELECT AD_InfoWindow_ID FROM AD_InfoWindow WHERE IsActive='Y' AND Value='" + InfoSearchKey + "'", null, null));
         }
 
     }
@@ -534,7 +897,44 @@ namespace VIS.Models
         }
     }
 
+    public class InfoSearchCol
+    {
+        public int AD_Reference_ID
+        {
+            get;
+            set;
+        }
 
+        public string ColumnName
+        {
+            get;
+            set;
+        }
+
+        public bool IsRange
+        {
+            get;
+            set;
+        }
+
+        public string SearchColumnName
+        {
+            get;
+            set;
+        }
+
+        public object Value
+        {
+
+            get; set;
+        }
+
+        public object ValueTo
+        {
+
+            get; set;
+        }
+    }
     //public class DisplayContent
     //{
 
