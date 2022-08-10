9a31e62bfa34fcd9717423f3de715c1207cad610
Amit Bansal <amit.bansal@viennasolutions.com>
2022-08-10 17:57:31 +0530

    From date not to be update on Balance Accumulation on Period close.

--- ModelLibrary/Process/PeriodStatus.cs
+++ ModelLibrary/Process/PeriodStatus.cs
@@ -117,35 +117,35 @@ namespace VAdvantage.Process
             // Lokesh Chauhan 17-Dec-2013
             /* Change For setting date From in Balance Aggregation window
               For Fact Account Balance updation */
-            if (no >= 0)
-            {
-                if (MPeriodControl.PERIODACTION_ClosePeriod.Equals(_PeriodAction))
-                {
-                    try
-                    {
-                        string sqlSchID = "SELECT C_AcctSchema_ID FROM C_AcctSchema WHERE IsActive = 'Y' AND AD_Client_ID = " + GetCtx().GetAD_Client_ID();
-                        DataSet ds = DB.ExecuteDataset(sqlSchID);
+            //if (no >= 0)
+            //{
+            //    if (MPeriodControl.PERIODACTION_ClosePeriod.Equals(_PeriodAction))
+            //    {
+            //        try
+            //        {
+            //            string sqlSchID = "SELECT C_AcctSchema_ID FROM C_AcctSchema WHERE IsActive = 'Y' AND AD_Client_ID = " + GetCtx().GetAD_Client_ID();
+            //            DataSet ds = DB.ExecuteDataset(sqlSchID);
 
-                        if (ds != null)
-                        {
-                            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                            {
-                                int C_AcctSchema_ID = Util.GetValueOfInt(ds.Tables[0].Rows[i]["C_AcctSchema_ID"]);
-                                string sqlUpd = "UPDATE Fact_Accumulation SET DateFrom = " + DB.TO_DATE(period.GetStartDate().Value.AddDays(-1)) + " WHERE IsActive = 'Y' AND AD_Client_ID = " + GetCtx().GetAD_Client_ID();
-                                no = DB.ExecuteQuery(sqlUpd, null, Get_TrxName());
-                                if (Get_Trx().Commit())
-                                {
+            //            if (ds != null)
+            //            {
+            //                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            //                {
+            //                    int C_AcctSchema_ID = Util.GetValueOfInt(ds.Tables[0].Rows[i]["C_AcctSchema_ID"]);
+            //                    string sqlUpd = "UPDATE Fact_Accumulation SET DateFrom = " + DB.TO_DATE(period.GetStartDate().Value.AddDays(-1)) + " WHERE IsActive = 'Y' AND AD_Client_ID = " + GetCtx().GetAD_Client_ID();
+            //                    no = DB.ExecuteQuery(sqlUpd, null, Get_TrxName());
+            //                    if (Get_Trx().Commit())
+            //                    {
                                     VAdvantage.Report.FinBalance.UpdateBalance(GetCtx(), C_AcctSchema_ID, period.GetStartDate().Value.AddDays(-1), Get_TrxName(), 0, this);
-                                }
-                            }
-                        }
-                    }
-                    catch
-                    {
+            //                    }
+            //                }
+            //            }
+            //        }
+            //        catch
+            //        {
 
-                    }
-                }
-            }
+            //        }
+            //    }
+            //}
 
             CacheMgt.Get().Reset("C_PeriodControl", 0);
             CacheMgt.Get().Reset("C_Period", _C_Period_ID);
