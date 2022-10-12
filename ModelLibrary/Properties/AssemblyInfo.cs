bc617489ebd8dda11004fcedab1e20a3d3e42cec
Pre Live <preliveserver@viennasolutions.com>
2022-10-12 10:28:23 +0000

    4.11.3.1

--- ModelLibrary/Properties/AssemblyInfo.cs
+++ ModelLibrary/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
 
-[assembly: AssemblyFileVersion("1.11.4.1")] 
+[assembly: AssemblyFileVersion("1.11.4.2")] 
 
